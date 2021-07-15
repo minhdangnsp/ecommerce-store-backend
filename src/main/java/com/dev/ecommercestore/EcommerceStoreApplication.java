67ee0497713c0db160e35ba72f52c460cd7b9e0f
minhdangnsp <dangnguyentranminh@gmail.com>
2021-07-15 09:12:46 +0700

    commit new project

--- src/main/java/com/dev/ecommercestore/EcommerceStoreApplication.java
+++ src/main/java/com/dev/ecommercestore/EcommerceStoreApplication.java
@@ -1,13 +1,34 @@
 package com.dev.ecommercestore;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
-public class EcommerceStoreApplication {
+@EnableAutoConfiguration(
+exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class EcommerceStoreApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CustomerRepository customerRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceStoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		List<Customer> customers = customerRepo.findAll();
+		
+		customers.forEach(System.out :: println);
+		
 	}
 
 }
